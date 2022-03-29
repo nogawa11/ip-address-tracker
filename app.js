60d10da3d9cbe34dfe866371b9680a44d80ea6b2
Nicole Ogawa <nogawa11@gmail.com>
2022-03-29 18:03:05 +0900

    refactored js functions

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
-const leafIcon = L.Icon.extend({
-});
+const form = document.querySelector('.form');
+const ipAddress = document.querySelector('.info-ipaddress')
+const input = document.querySelector('.input');
+const isp = document.querySelector('.info-isp');
+const region = document.querySelector('.info-location')
+const submit = document.querySelector('.button-submit');
+const timezone = document.querySelector('.info-timezone');
+
+const ipifyKey = config.IPIFY_KEY;
+const mapboxKey = config.MAPBOX_KEY;
+
+const leafIcon = L.Icon.extend({});
 const mapIcon = new leafIcon({
   iconUrl: 'images/icon-location.svg',
   iconSize:     [38, 45],
   iconAnchor:   [22, 45],
   popupAnchor:  [-3, -76]
 })
+
 const mymap = L.map('map')
 const marker = L.marker([0, 0], {icon: mapIcon}).addTo(mymap);
-const ipAddress = document.querySelector('.info-ipaddress')
-const region = document.querySelector('.info-location')
-const timezone = document.querySelector('.info-timezone');
-const isp = document.querySelector('.info-isp');
-const submit = document.querySelector('.button-submit');
-const form = document.querySelector('.form');
-const input = document.querySelector('.input');
-const mapboxKey = config.MAPBOX_KEY;
-const ipifyKey = config.IPIFY_KEY;
 
 const displayMap = (lat, long) => {
   mymap.setView([lat, long], 13);
@@ -34,19 +36,18 @@ const displayMap = (lat, long) => {
 
 const displayCurrentLocation = () => {
   const response = fetch(`https://geo.ipify.org/api/v1?apiKey=${ipifyKey}`, { method: 'GET' })
-                .then(response => response.json())
-                .catch(() => console.error('Request failed'));
+                  .then(response => response.json())
+                  .catch(() => console.error('Request failed'));
   response.then(response => {
-      const lat = response.location.lat;
-      const lng = response.location.lng;
-
-      ipAddress.innerHTML = response.ip;
-      region.innerHTML = response.location.city;
-      timezone.innerHTML = `UTC ${response.location.timezone}`;
-      isp.innerHTML = response.isp;
-      displayMap(lat, lng);
-    })
-    .catch(() => console.error('Request failed'));
+    const lat = response.location.lat;
+    const lng = response.location.lng;
+    ipAddress.innerHTML = response.ip;
+    region.innerHTML = response.location.city;
+    timezone.innerHTML = `UTC ${response.location.timezone}`;
+    isp.innerHTML = response.isp;
+    displayMap(lat, lng);
+  })
+  .catch(() => console.error('Request failed'));
 }
 
 displayCurrentLocation();
@@ -55,20 +56,18 @@ const getLocation = () =>  {
   form.addEventListener('submit', (event) => {
     event.preventDefault();
     const response = fetch(`https://geo.ipify.org/api/v1?apiKey=${ipifyKey}&ipAddress=${input.value}`)
-                  .then(res => res.json())
-                  .catch(() => console.error("Request failed"));
+                    .then(res => res.json())
+                    .catch(() => console.error("Request failed"));
     response.then(response => {
-        const lat = response.location.lat;
-        const lng = response.location.lng;
-
-        ipAddress.innerHTML = response.ip;
-        region.innerHTML = response.location.city;
-        timezone.innerHTML = `UTC ${response.location.timezone}`;
-        isp.innerHTML = response.isp;
-
-        displayMap(lat, lng);
-      })
-      .catch(() => console.error('Request failed'));
+      const lat = response.location.lat;
+      const lng = response.location.lng;
+      ipAddress.innerHTML = response.ip;
+      region.innerHTML = response.location.city;
+      timezone.innerHTML = `UTC ${response.location.timezone}`;
+      isp.innerHTML = response.isp;
+      displayMap(lat, lng);
+    })
+    .catch(() => console.error('Request failed'));
   });
 }
 
